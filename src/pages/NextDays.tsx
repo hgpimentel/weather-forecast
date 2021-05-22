002d209e877eeb538caf1e5fcd7d4a763d33664d
hgpimentel <hugo.g.pimentel@gmail.com>
2021-05-22 09:42:33 +0100

    WF-5 - page templates

--- src/pages/NextDays.tsx
+++ src/pages/NextDays.tsx
@@ -1,10 +1,37 @@
+import styled from "styled-components";
+import { CenteredContainer, ColumnContainer } from "../components";
+
+const NextDaysContainer = styled(ColumnContainer)`
+  margin: 0 20%;
+  padding: 2rem;
+  border: 5px solid teal;
+  border-radius: 1rem;
+`;
+
+const CustomCenteredContainer = styled(CenteredContainer)`
+  align-items: center;
+  justify-content: space-around;
+  padding: 1rem;
+  border-bottom: 2px solid teal;
+`;
+
 const NextDays: React.FC = () => {
   return (
-    <>
-      <div>
-        <h1>NEXT 7 DAYS FORECAST</h1>
-      </div>
-    </>
+    <NextDaysContainer>
+      {Array(7)
+        .fill(1)
+        .map((a, index) => {
+          return (
+            <CustomCenteredContainer key={index}>
+              <div>Day</div>
+              <div>Min/Max</div>
+              <div>Sky</div>
+              <div>Rain %</div>
+              <div>Wind</div>
+            </CustomCenteredContainer>
+          );
+        })}
+    </NextDaysContainer>
   );
 };
 
--- src/pages/Today.tsx
+++ src/pages/Today.tsx
@@ -1,10 +1,36 @@
+import styled from "styled-components";
+import { CenteredContainer, ColumnContainer } from "../components";
+
+const TodayContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0 20%;
+  padding: 2rem;
+  border: 5px solid teal;
+  border-radius: 1rem;
+`;
+
+const CustomCenteredContainer = styled(CenteredContainer)`
+  align-items: center;
+`;
+
 const Today: React.FC = () => {
   return (
-    <>
-      <div>
-        <h1>TODAY FORECAST</h1>
-      </div>
-    </>
+    <TodayContainer>
+      <ColumnContainer>
+        <div>Time</div>
+        <div>
+          <h2>Temperature</h2>
+        </div>
+        <div>
+          <h3>Sky</h3>
+        </div>
+        <div>Rain %</div>
+      </ColumnContainer>
+      <CustomCenteredContainer>
+        <div>Min/max Temp</div>
+      </CustomCenteredContainer>
+    </TodayContainer>
   );
 };
 
